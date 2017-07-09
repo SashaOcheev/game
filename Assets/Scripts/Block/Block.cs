59ec28585e07e9a79d0d260c1a9d65690dc1e564
SashaOcheev <OcheevSasha@gmail.com>
2017-07-09 23:55:57 +0300

    block rotation done!

--- Assets/Scripts/Block/Block.cs
+++ Assets/Scripts/Block/Block.cs
@@ -41,8 +41,19 @@ namespace Scripts.Block
                     CurrentPosition = Position.Y;
                     break;
                 case Position.Y:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x + transform.localScale.y / 2, transform.position.y - transform.localScale.x / 2, transform.position.z),
+                        new Vector3(0f, 0f, -1f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.X;
                     break;
                 case Position.Z:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x + transform.localScale.y / 2, transform.position.y - transform.localScale.y / 2, transform.position.z),
+                        new Vector3(0f, 0f, -1f),
+                        DeltaAngle
+                    );
                     break;
                 default:
                     throw new Exception("Undefined block Position");
@@ -62,8 +73,19 @@ namespace Scripts.Block
                     CurrentPosition = Position.Y;
                     break;
                 case Position.Y:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x - transform.localScale.y / 2, transform.position.y - transform.localScale.x / 2, transform.position.z),
+                        new Vector3(0f, 0f, 1f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.X;
                     break;
                 case Position.Z:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x - transform.localScale.y / 2, transform.position.y - transform.localScale.y / 2, transform.position.z),
+                        new Vector3(0f, 0f, 1f),
+                        DeltaAngle
+                    );
                     break;
                 default:
                     throw new Exception("Undefined block Position");
@@ -82,8 +104,20 @@ namespace Scripts.Block
                     );
                     break;
                 case Position.Y:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x, transform.position.y - transform.localScale.x / 2, transform.position.z + transform.localScale.z / 2),
+                        new Vector3(1f, 0f, 0f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.Z;
                     break;
                 case Position.Z:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x, transform.position.y - transform.localScale.y / 2, transform.position.z + transform.localScale.x / 2),
+                        new Vector3(1f, 0f, 0f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.Y;
                     break;
                 default:
                     throw new Exception("Undefined block Position");
@@ -102,8 +136,20 @@ namespace Scripts.Block
                     );
                     break;
                 case Position.Y:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x, transform.position.y - transform.localScale.x / 2, transform.position.z - transform.localScale.z / 2),
+                        new Vector3(-1f, 0f, 0f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.Z;
                     break;
                 case Position.Z:
+                    transform.RotateAround(
+                        new Vector3(transform.position.x, transform.position.y - transform.localScale.y / 2, transform.position.z - transform.localScale.x / 2),
+                        new Vector3(-1f, 0f, 0f),
+                        DeltaAngle
+                    );
+                    CurrentPosition = Position.Y;
                     break;
                 default:
                     throw new Exception("Undefined block Position");
